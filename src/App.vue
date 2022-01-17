cb8dae56b09f37ae8b48a572e1f3c1cdbff928e2
maxlz <65449783+Erdoulove@users.noreply.github.com>
2022-01-17 15:11:46 +0800

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <Main/>
+  </div>
+</template>
+
+<script>
+import Main from './components/Main.vue'
+
+export default {
+  name: 'app',
+  components: {
+    Main
+  }
+}
+</script>
+
+<style>
+body {
+  margin: 0px;
+}
+#app {
+  font-family: "Microsoft YaHei", 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  height: 100%;
+  width: 100%;
+  position: absolute;
+  color: black;
+}
+</style>
--- src/main.js
+++ src/main.js
@@ -1,1 +1,14 @@
+import Vue from 'vue'
+import './plugins/axios'
+import App from './App.vue'
+import './plugins/element.js'
+import axios from "axios";
 
+Vue.config.productionTip = false
+// Vue.forceUpdate()
+
+axios.defaults.baseURL = 'http://192.168.2.10:5000/'
+
+new Vue({
+  render: h => h(App),
+}).$mount('#app')
